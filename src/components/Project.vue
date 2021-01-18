4c4c41e06be5fe9fe8cd509f3ddb88fd58c52a94
Jason Melis <jason.melis@gmail.com>
2021-01-18 15:20:48 -0500

    Playing around with adding a component

---
+++ src/components/Project.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>This is a Vue component</h1>
+  <h3>{{ name }}</h3>
+</template>
+
+<script>
+export default {
+  name: 'Project',
+  props: {
+    name: String,
+  }
+}
+</script>
